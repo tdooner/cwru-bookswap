f9c6259395919893bdd701400ea1cfe04e008c9c
Tom Dooner <tomdooner@gmail.com>
2010-12-26 02:22:45 -0500

    Version that is running against bookswap and bookscouter now.

--- bookswap.py
+++ bookswap.py
@@ -8,29 +8,31 @@ DEPENDENCIES:
 
 import urllib, re
 import libxml2dom
+import sys, time
 
 NUMBER_VENUES = 2 # Number of sites to show from BookScouter for an item.
 
 letterstosearch = ['+']
-bookids = ['18405','12710'] # Eventually this will be an empty array
+bookids = [] # Eventually this will be an empty array
 
-print("Pulling Bookswap Database")
-'''
-Commenting out for now because I want to minimize the pain on their server
-
+#print("Pulling Bookswap Database")
 for c in letterstosearch:
 	f = urllib.urlopen("http://bookswap.case.edu/search.php?title="+c+"&sort=price&order=desc")
 	bookids = re.findall('(?<=bookdata\.php\?bookid=)([0-9]*)', f.read())
 	f.close()
-'''
 
-print("Found " + str(len(bookids)) + " results. Pulling data...")
+#print("Found " + str(len(bookids)) + " results. Pulling data...")
 
 for i in bookids:
 	f = urllib.urlopen("http://bookswap.case.edu/bookdata.php?bookid="+i)
 	html = f.read()
 	price = re.search("(?<=Price:</font> \$)([0-9\.]*)", html).group(0)
-	isbn = re.search("(?<=ISBN-10:</font> )[0-9]{10}", html).group(0)
+	isbn = re.search("(?<=ISBN-10:</font> )[0-9X]{10}", html)
+	if isbn:
+		isbn = isbn.group(0)
+	else:
+		sys.stderr.write("Error: No ISBN Found for " + i)
+		continue
 	print(isbn + " " + price), # This comma trick is the stupidest thing ever.
 	g = urllib.urlopen("http://m.bookscouter.com/prices.php?isbn="+isbn)
 	html = libxml2dom.parseString(g.read(), html=1)
@@ -41,3 +43,4 @@ for i in bookids:
 		price  = tablecells[n+1].xpath(".//a")[0].textContent
 		print(name + " " + str(price)),
 	print("")
+	time.sleep(0.5)
